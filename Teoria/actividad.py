85607ae38c4b5f240f3068bcd5159f140e4056d5
Jose Ignacio <joseborr8@hotmail.com>
2021-04-30 13:56:50 -0300

    divido en modulos y se completa consigna para uno de los datasets

--- Teoria/actividad.py
+++ Teoria/actividad.py
@@ -1,35 +1,21 @@
 import ventanas
-import csv
-import os
-import json
+import funciones
 import collections
-import ventanas
+
 
 #fuente https://ourworldindata.org/life-expectancy
-path = 'files'
-archivo_exp = 'life-expectancy.csv'
-try: 
-    path_arch = os.path.join(os.getcwd(),path)
-    archivo = open(os.path.join(path_arch,archivo_exp),'r')
-    csvreader = csv.reader(archivo,delimiter=',')
-except:
-    print('No se pudo abrir el archivo ' + archivo_exp)
-header, lista = next(csvreader),list(csvreader)
-
-def continente(cadena):
-    '''devuelve boolean si la cadena ingresada es continente'''
-    continentes = ['Africa','Americas','Asia','Europe','Oceania']
-    return cadena in continentes
-    
+header, lista = funciones.abrir_archivo('files','life-expectancy.csv') 
 
 lista = list(map(lambda y:[y[0],float(y[3])],filter(lambda x:x[2]== '2019' 
-                                                        and not continente(x[0]),lista)))
+                                                        and not funciones.continente(x[0]),lista)))
 
 c = collections.Counter
 mayor_exp = {}
 for elem in lista:
     mayor_exp[elem[0]] = elem[1]
-mayor_exp = c(mayor_exp).most_common(20) #los 20 paises que mayor expectativa de vida tienen en 2019
+mayor_exp = c(mayor_exp).most_common(20) #los 20 estados que mayor expectativa de vida tienen en 2019
+
+dic_mayor_exp = dict(mayor_exp)
+funciones.crear_json('files','expectativa.txt',dic_mayor_exp)
+
 ventanas.ventana_principal(mayor_exp)
-
-
--- Teoria/ventanas.py
+++ Teoria/ventanas.py
@@ -1,7 +1,8 @@
 import PySimpleGUI as sg 
 
 def ventana_principal(mayor_exp):
-    layout = [[sg.Button('20 paises con mayor expectativa de vida',size=(50,2),key='-BOTON 1-')],
+    '''Ventana principal del programa'''
+    layout = [[sg.Button('Expectativa de vida',size=(50,2),key='-BOTON 1-')],
                 [sg.Button('Boton 2',size=(50,2), key='-BOTON 2-')],
                 [sg.Text(text='')],
                 [sg.Button('Salir',size=(50,2),key='-SALIR-')]]
@@ -22,7 +23,8 @@ def ventana_principal(mayor_exp):
     window.close()
 
 def ventana_Boton1(mayor_exp):
-    layout = [[sg.Text('20 paises ordenados segun expectativa de vida: '),sg.Combo(values=(mayor_exp))],
+    '''Ventana que se abre luego de presionar boton 1 en ventana principal'''
+    layout = [[sg.Text('20 estados con mayor expectativa de vida: '),sg.Combo(values=(mayor_exp))],
                 [sg.Button('Volver',key='-VOLVER-')]]
     window = sg.Window('Boton 1',layout)
     
@@ -33,6 +35,7 @@ def ventana_Boton1(mayor_exp):
     window.close()
 
 def ventana_Boton2():
+    '''Ventana que se abre luego de presionar boton 2 en ventana principal'''
     layout = [[sg.Text(text='aca va el layout del boton 2')],
                 [sg.Button('Volver',key='-VOLVER-')]]
     window = sg.Window('Boton 1',layout)
